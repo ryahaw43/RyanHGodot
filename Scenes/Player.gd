88adde50727cbd7aa34b3e8f2bfcb71a7ec2ddba
ryahaw43 <ryahaw43@kingshigh.school.nz>
2022-08-02 16:28:49 +1200

    did a lot of stuff

---
+++ Scenes/Player.gd
@@ -0,0 +1,49 @@
+extends KinematicBody
+
+#basic variables
+var velocity = Vector3()
+var gravity = -30
+var max_speed = 8
+var mouse_sensitivity = 0.002
+
+#gun variables
+
+
+#functions
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
+func get_input():
+	var input_dir =Vector3()
+	if Input.is_action_pressed("move_forward"):
+		input_dir += -global_transform.basis.z
+	if Input.is_action_pressed("move_back"):
+		input_dir -= -global_transform.basis.z
+	if Input.is_action_pressed("strafe_left"):
+		input_dir += -global_transform.basis.x
+	if Input.is_action_pressed("strafe_right"):
+		input_dir += +global_transform.basis.x
+	input_dir = input_dir.normalized() #add to cancel strafe running
+	return input_dir
+	
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * mouse_sensitivity)
+		$Pivot.rotate_x(-event.relative.y * mouse_sensitivity)
+		$Pivot.rotation.x =clamp($Pivot.rotation.x,-1.2,1.2)
+		
+func _physics_process(delta):
+	#gravity
+	velocity.y += gravity * delta
+	var desired_velocity = get_input() * max_speed
+	velocity.x = desired_velocity.x
+	velocity.z = desired_velocity.z
+	velocity = move_and_slide(velocity, Vector3.UP, true)
+	
+	
+func change_gun(gun):
+	pass
+	
+func _process(delta):
+	pass
+	
